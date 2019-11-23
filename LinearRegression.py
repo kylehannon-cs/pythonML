b7f9d5811ad0c052514d19d056695577425267e5
Kyle Hannon <hannonk@oregonstate.edu>
2019-11-23 14:22:52 -0800

    Starting ploting and using pickle

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,7 +4,9 @@ import sklearn
 from sklearn import model_selection
 from sklearn import linear_model
 from sklearn.utils import shuffle
-
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 data = pd.read_csv("student-mat.csv", sep=';')
 
@@ -17,11 +19,26 @@ y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+"""
+best = 0
+for _ in range(30):
 
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+	x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
+
+	linear = linear_model.LinearRegression()
+
+	linear.fit(x_train, y_train)
+	acc = linear.score(x_test, y_test)
+	print(acc) 
+
+	if acc > best:
+		best = acc
+		with open("studentmodel.pickle", "wb") as f:
+			pickle.dump(linear, f)
+"""
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print("Coef: \n" , linear.coef_)
 print("Intercept:  \n", linear.intercept_)
@@ -30,3 +47,23 @@ predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
 	print(predictions[x], x_test[x], y_test[x])
+
+p = "absences"
+style.use("ggplot")
+pyplot.scatter(data[p], data[predict])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
