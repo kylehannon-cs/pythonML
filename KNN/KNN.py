11bdc4515dd800a58da2e5f6d1e2c35b0994f95c
Kyle Hannon <hannonk@oregonstate.edu>
2019-12-17 20:42:47 -0800

    Working KNN

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -25,5 +25,17 @@ y = list(cls)
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1)
 
-print(x_train, y_test)
 
+model = KNeighborsClassifier(n_neighbors = 7)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+predicted = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for x in range(len(predicted)):
+	print("Predicted: ", names[predicted[x]], "Data: ", x_test[x], "Actual: ", names[y_test[x]])
+	n = model.kneighbors([x_test[x]], 7, True)
+	print("N:", n)
